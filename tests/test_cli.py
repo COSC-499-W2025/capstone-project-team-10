3cf42249f245168c98951c5bde019587b45764ac
Adam Badry <57472142+Sk3tch7y@users.noreply.github.com>
2025-10-19 14:05:48 -0700

    added tests for zip file parsing
    Add zip tests to ensure that zip files are extracted properly

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,8 @@
 import pytest
 import src.cli.cli_app as cli
 from unittest.mock import patch
+
+path_to_test_zip = "tests/testdata/test_cli/testScanFolder.zip"
 class TestCli:
     def test_cli_start_no_file(self):
         pos_test_args = ["--cli", "-y"]
@@ -15,3 +17,10 @@ class TestCli:
             cli.run_cli(neg_test_args)
             #TODO: update with expected cli response from no -y flag
             mock_print.assert_called_with("Hello from CLI!")
+
+    def test_cli_parse_zip(self):
+        pos_test_args = ["--cli", "-y", "--zip ${path_to_test_zip}"]
+        with patch('builtins.print') as mock_print:
+            cli.run_cli(pos_test_args)
+            #TODO: update with cli response expected for a successful parse, currently uses the file count parsed
+            mock_print.assert_called_with("2")
---
+++ tests/test_zip.py
@@ -0,0 +1,13 @@
+import pytest
+import src.zip.zip_app as zip
+import src.fss.fss as fss
+from unittest.mock import patch
+
+path_to_test_zip = "tests/testdata/test_zip/testScanFolder.zip"
+
+
+class TestZip:
+    def test_zip_parse_zip(self):
+        result = zip.extract_zip(path_to_test_zip)
+        # TODO: update with zip response expected for a successful extract, currently uses the fss to scan and verify the file count, this verifies that the file is findable and contains the proper files
+        assert fss.search(result) == 2
