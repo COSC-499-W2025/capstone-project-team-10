c92ed1d1188299b108812f940dc3fd1b9938dc6e
Adam Badry <57472142+Sk3tch7y@users.noreply.github.com>
2025-10-19 13:56:55 -0700

    Drafted Test cases for CLI, FSS, and param/config features
    created draft test cases and test data for the CLI, FSS, and Config features currently on the backlog

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,8 +2,16 @@ import pytest
 import src.cli.cli_app as cli
 from unittest.mock import patch
 class TestCli:
-    def test_cli_start(self):
+    def test_cli_start_no_file(self):
+        pos_test_args = ["--cli", "-y"]
         with patch('builtins.print') as mock_print:
-            cli.run_cli()
+            cli.run_cli(pos_test_args)
+            #TODO: update with cli response from no file passed in
             mock_print.assert_called_with("Hello from CLI!")
 
+    def test_cli_start_no_y(self):
+        neg_test_args = ["--cli"]
+        with patch('builtins.print') as mock_print:
+            cli.run_cli(neg_test_args)
+            #TODO: update with expected cli response from no -y flag
+            mock_print.assert_called_with("Hello from CLI!")
