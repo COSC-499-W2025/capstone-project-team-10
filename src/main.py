54def7dc23cb62e0207456a3780acff8000d8590
A-Shrew <122584869+A-Shrew@users.noreply.github.com>
2025-10-19 17:39:56 -0700

    Added -y flag and user permission request if flag is not true

--- src/main.py
+++ src/main.py
@@ -3,8 +3,18 @@ from gui.gui_app import run_gui
 from cli.cli_app import run_cli
 
 def main():
+
     if len(sys.argv) > 1 and sys.argv.__contains__("--cli"):
+        if not sys.argv.__contains__("-y"):
+            
+            # Request User Permission to Access Files
+            print("This application requires access to system files")
+            response = input("Do wish to continue? (Y/N): ")
+            if response.lower() != 'y':
+                sys.exit(1)
+
         run_cli()
+        
     else:
         run_gui()
 
