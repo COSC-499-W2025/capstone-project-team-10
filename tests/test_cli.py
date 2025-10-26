1bd7131c46d1db024b7040a1a423d4ac76691db6
Adam Badry <57472142+Sk3tch7y@users.noreply.github.com>
2025-10-26 11:51:37 -0700

    Created --zip flag and refactored
    Changes made
    - refactored -y check into run_cli instead of main.py
    - created functions for getting and returning zip files
    - created function to extract the param at a given spot and checking if it is a key or a param body
    - created -q flag to disable system output during operation and re-enable when execution has finished and results are ready
    - Added tests to verify cli argument parsing
    - Added test scan folder

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,24 +3,36 @@ import src.cli.cli_app as cli
 from unittest.mock import patch
 
 path_to_test_zip = "tests/testdata/test_cli/testScanFolder.zip"
+path_to_test_folder = "tests/testdata/test_cli/testScanFolder"
 class TestCli:
     def test_cli_start_no_file(self):
         pos_test_args = ["--cli", "-y"]
-        with patch('builtins.print') as mock_print:
+        with pytest.raises(SystemExit) as exc_info:
             cli.run_cli(pos_test_args)
-            #TODO: update with cli response from no file passed in
-            mock_print.assert_called_with("Hello from CLI!")
+        # Check the exit code
+        assert exc_info.value.code == 1
 
-    def test_cli_start_no_y(self):
-        neg_test_args = ["--cli"]
-        with patch('builtins.print') as mock_print:
-            cli.run_cli(neg_test_args)
-            #TODO: update with expected cli response from no -y flag
-            mock_print.assert_called_with("Hello from CLI!")
+    def test_cli_user_response(self):
+        pos_test_args = ["--cli" , path_to_test_folder]
+        with patch('builtins.input', return_value="Y"), patch('builtins.print') as mock_print:
+            cli.run_cli(pos_test_args)
+            # Check the exit code
+            mock_print.assert_called_with("Scanning file path: " + path_to_test_folder)
+
+    def test_cli_user_response_no(self):
+        pos_test_args = ["--cli", path_to_test_folder]
+        with patch('builtins.input', return_value="N"), patch('builtins.print') as mock_print:
+            with pytest.raises(SystemExit) as exc_info:
+                cli.run_cli(pos_test_args)
+            # Check the exit code
+            assert exc_info.value.code == 1
 
     def test_cli_parse_zip(self):
-        pos_test_args = ["--cli", "-y", "--zip ${path_to_test_zip}"]
-        with patch('builtins.print') as mock_print:
-            cli.run_cli(pos_test_args)
-            #TODO: update with cli response expected for a successful parse, currently uses the file count parsed
-            mock_print.assert_called_with("2")
+        pos_test_args = ["--cli", "-y", "--zip", path_to_test_zip]
+        result: str = cli.get_param_body(pos_test_args, pos_test_args.index("--zip") + 1)
+        assert result == path_to_test_zip
+
+    def test_cli_parse_zip_missing_path(self):
+        pos_test_args = ["--cli", "--zip", "-y", path_to_test_folder]
+        result: str = cli.get_param_body(pos_test_args, pos_test_args.index("--zip") + 1)
+        assert result != path_to_test_zip
