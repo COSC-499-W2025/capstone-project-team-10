dbeb17b4e133bc83254bd3f8f389559b07898cc5
Abdalla Haider Abbas <abendos203@gmail.com>
2025-10-26 17:28:11 -0700

    Added more comments for clarity

--- src/zip/zip_app.py
+++ src/zip/zip_app.py
@@ -16,10 +16,13 @@ def extract_zip(origin_path: str | Path):
         print("Error: The provided file is not a valid ZIP file.")
         return None
     
-    time_tag = datetime.now().strftime("%Y%m%d-%H%M%S-%f")[:-3] #gets current date and time for the tag
-    destination_path = base_destination_path / f"zip_{time_tag}"
+    # gets current date and time for the tag
+    time_tag = datetime.now().strftime("%Y%m%d-%H%M%S-%f")[:-3]
+    # sends the zip file to its designated folder in the zipfolders folder
+    # the the name "zip_########-######-###" where the digits are the current date and time
+    destination_path = base_destination_path / f"zip_{time_tag}" 
 
-    try:
+    try: # extracts zip
         with zipfile.ZipFile(origin_path, 'r') as zip_ref:
             zip_ref.extractall(destination_path)
 
@@ -31,11 +34,16 @@ def extract_zip(origin_path: str | Path):
         print("Error: ZIP file not found")
         return None
     
+    # sets the pattern to "zip_########-######-###" in order to match
+    # with the folders later and delete the oldest one
     filename_pattern = re.compile(rf"^zip_\d{{8}}-\d{{6}}-\d{{3}}$")
-    
+    # finds all files in zipfolder which match the naming pattern
+    # and sorts them in reverse
     zipFolders = [files for files in base_destination_path.iterdir() if files.is_dir() and filename_pattern.match(files.name)]
     zipFolders.sort(key=lambda file: file.name, reverse=True)
-    for oldFiles in zipFolders[5:]: 
-        shutil.rmtree(oldFiles, ignore_errors=True)
+
+    # the newest 5 folders are kept and the rest are deleted
+    for old_Files in zipFolders[5:]: 
+        shutil.rmtree(old_Files, ignore_errors=True)
     
     return base_destination_path
