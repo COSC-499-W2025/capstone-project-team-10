2f6c369819ce797f3b053e83f955559794bbbaf3
Adam Badry <57472142+Sk3tch7y@users.noreply.github.com>
2025-10-30 14:46:10 -0700

    Fixed Integration withzip and FSS modules
    - updated function calls to be compatible with expect inputs
    - updated tests to fix fss output interfering with Cli tests

--- src/cli/cli_app.py
+++ src/cli/cli_app.py
@@ -1,26 +1,30 @@
 import sys
-#from zip.zip import zip
-#from fss.fss import fss
+import src.zip.zip_app as zip
+import src.fss.fss as fss
 import builtins
+
 
 def prompt_file_perms():
     # Request User Permission to Access Files
     print("This application requires access to system files")
     response = input("Do wish to continue? (Y/N): ")
-    if response.lower() != 'y':
+    if response.lower() != "y":
         sys.exit(1)
 
+
 def extract_chosen_zip(zip_file_path: str):
-    #TODO: Link fss call here to assign the result to file_path
-    file_path = zip.zip_extract(zip_file_path) 
-    print("File unzipped at: " + file_path)
+    # TODO: Link fss call here to assign the result to file_path
+    file_path = zip.extract_zip(zip_file_path)
+    print("File unzipped at: " + str(file_path))
     return file_path
+
 
 def get_param_body(args: list[str], indx: int) -> str:
     if indx < len(args) and not args[indx].startswith("-"):
         return args[indx]
     else:
         return ""
+
 
 def run_cli(cli_args: list[str]):
     quiet: bool = ("--quiet" in cli_args) or ("-q" in cli_args)
@@ -40,19 +44,19 @@ def run_cli(cli_args: list[str]):
         if zip_file == "":
             print("Expected File path following --zip")
             sys.exit(1)
-        file_path = extract_chosen_zip(zip_file)
+        file_path = str(zip.extract_zip(zip_file))
     else:
-        #TODO: Get folder to start search at.
-        file_path = get_param_body(cli_args, len(cli_args)-1)
-    if(file_path == ""):
+        # TODO: Get folder to start search at.
+        file_path = get_param_body(cli_args, len(cli_args) - 1)
+    if file_path == "":
         print("No starting file_path")
         sys.exit(1)
     print("Scanning file path: " + file_path)
-    #TODO: Link fss call here with completed path
+    # TODO: Link fss call here with completed path
     fss.search(file_path, path_exclusions)
 
-    #Reactivate prints if turned off
+    # Reactivate prints if turned off
     if quiet:
         builtins.print = _original_print
 
-    #TODO: get most recent log file and print it for consumption
+    # TODO: get most recent log file and print it for consumption
