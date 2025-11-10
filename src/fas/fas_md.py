91b70a96aec48ef648bae90e2b4c94ae11dcd757
notbaopham <113058628+notbaopham@users.noreply.github.com>
2025-11-09 18:17:30 -0800

    [B] Added more functions, updating comments

--- src/fas/fas_md.py
+++ src/fas/fas_md.py
@@ -63,9 +63,54 @@ class Markdown:
 
         return root
 
+    def get_word_counts(self):
+        """
+            Return the numbers of word counts within the .md
+
+            Output:
+                (int) number of word counts within the .md
+        """
+        return self.analyzer.count_words()
+
+    def get_code_blocks(self):
+        """
+            Return all the code blocks within the .md
+
+            Output:
+                (dict) as shown in the example
+
+            Example:
+                print(test_markdown.get_code_blocks())
+                >> {'Code block':
+                    [{'start_line': 47, 'content': 'import pandas as pd\ndf = pd.read_csv("data/input.csv")\ndf.head()', 'language': 'python'},
+                    {'start_line': 63, 'content': 'summary(df)\nplot(df$value)', 'language': 'r'}]}
+        """
+        return self.analyzer.identify_code_blocks()
+
+    def get_paragraphs(self):
+        """
+            Return all the paragraphs within the .md
+
+            Output:
+                (dict) as shown in the example
+
+            Example:
+                print(test_markdown.get_paragraphs())
+                >>
+                {'Paragraph':
+                ['This document tests **mrkdown-analysis** parsing capabilities.',
+                '`mrkdown-analysis` aims to:',
+                'Markdown is a lightweight format.  \nExample inline code: `print("Hello, world!")`',
+                '![Chart Example](https://dummyimage.com/600x400/000/fff&text=Chart+Placeholder)']}
+        """
+        return self.analyzer.identify_paragraphs()
+
+    # Note - there is so much more that markdown-analysis can do - but there's also a few that it cannot do.
+    # Have to congregate a meetings to discuss what (more) is needed to extract from an .md, but for now, this should do.
+
 """
 # Usage
 test_markdown = Markdown("test_markdown.md")
-hierarchy = test_markdown.get_headers()
+hierarchy = test_markdown.get_paragraphs()
 print(hierarchy)
 """
