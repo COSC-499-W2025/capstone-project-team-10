9caf397776bac69463c35810be7865abafa3d532
notbaopham <113058628+notbaopham@users.noreply.github.com>
2025-11-16 20:27:58 -0800

    [B] Test suite implemented + changes of comments

--- src/log/log_converter.py
+++ src/log/log_converter.py
@@ -2,22 +2,26 @@ import os
 import pandas as pd
 
 class LogConverter:
-
+    """
+        Storing both the paths and file contents
+    """
     def __init__(self, path):
         self.__log_path = path
         self.csv = pd.read_csv(self.__log_path)
 
     def convert_to_JSON(self):
+        """
+            Convert (and appends) the converted log file in standard .JSON format
+        """
         json_str = self.csv.to_json(orient='records', indent=2)
 
-        # "file.csv" → "file_converted.json"
         base, _ = os.path.splitext(self.__log_path)
-        json_path = f"{base}_converted.json"
+        json_path = f"{base}_converted.json" # Differentiate the name, in case of funniness
 
         with open(json_path, "w") as f:
             f.write(json_str)
 
-        return json_path  # location of converted file
+        return json_path
 
     def convert_to_md(self):
         return None
