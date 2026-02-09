660f0079d7aa340dda216ce5af1ddabb54eb4def
notbaopham <113058628+notbaopham@users.noreply.github.com>
2026-02-08 19:54:43 -0800

    [B] gui_scan_manager.py updated

--- src/gui/gui_scan_manager.py
+++ src/gui/gui_scan_manager.py
@@ -1,21 +1,28 @@
 from src.fas.fas import FileAnalysis as fas
-from src.fss.fss import FSS_Search as fss
+from src.fss.fss import FSS_Search, search
 
 class ScanManager:
     
     def __init__(self):
-        
-        self.fas = None
         self.fss = None
+        self.last_result = None
     
-    def scan(self, directory_path):
+    def scan(self, directory_path, filters: dict):
         """
-          Placeholder method to initiate scan on the given directory
+          Initiate scan on the given directory using provided filters.
         """
-        pass
+        search_params = FSS_Search(
+            input_path=directory_path,
+            excluded_path=filters.get('excluded_paths', set()),
+            file_types=filters.get('file_types', set()),
+            time_lower_bound=filters.get('time_lower_bound', None),
+            time_upper_bound=filters.get('time_upper_bound', None),
+        )
+        self.last_result = search(search_params)
+        return self.last_result
     
     def get_scan_results(self):
         """
-          Placeholder method to retrieve scan results
+          Retrieve last scan results (currently total files scanned).
         """
-        pass
+        return self.last_result
